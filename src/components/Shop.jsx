3265703b858591d4f916b1d15f6b641f6c19357d
Chris Bentley <chrisrbentley@pm.me>
2023-03-22 22:28:32 -0400

    Products are fetched from API when shop component mounts

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from './Header';
 
 function Shop() {
+  const [products, setProducts] = useState([]);
+
+  async function getProducts() {
+    const response = await fetch('https://api.storerestapi.com/products', {
+      mode: 'cors',
+    });
+    const productData = await response.json();
+    setProducts(productData.data);
+    console.log(productData);
+  }
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
   return (
     <div>
       <Header />
