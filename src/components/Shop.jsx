4d84f7de15f2eabe0b8f23dfc3e5437b259e99af
Chris Bentley <chrisrbentley@pm.me>
2023-03-30 21:02:40 -0400

    Changed API

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,16 +1,27 @@
 import React, { useState, useEffect } from 'react';
 import Header from './Header';
 
-function Shop() {
+function Shop(props) {
+  const { cart } = props;
+
   const [products, setProducts] = useState([]);
 
   async function getProducts() {
-    const response = await fetch('https://dummyjson.com/products', {
+    /* const response = await fetch('https://api.escuelajs.co/api/v1/products', {
+      mode: 'cors',
+    }); */
+
+    const response = await fetch('https://fakestoreapi.com/products', {
       mode: 'cors',
     });
+
     const productData = await response.json();
-    setProducts(productData.products);
-    console.log(productData.products);
+    console.log(productData);
+    setProducts(productData);
+
+    /* const slicedData = productData.slice(0, 20);
+    setProducts(slicedData);
+    console.log(slicedData); */
   }
 
   useEffect(() => {
@@ -18,24 +29,28 @@ function Shop() {
   }, []);
 
   return (
-    <div>
-      <Header />
-      This is the shop
-      <div id="products">
-        {products.map((product) => {
-          return (
-            <div className="product">
-              <h3>{product.title}</h3>
-              <img
-                src={product.images[0]}
-                alt=""
-                className="product-img"
-              />
-              <p>${product.price}</p>
-            </div>
-          );
-        })}
-      </div>
+    <div id="store-container">
+      <Header cart={cart} />
+      <main id="p-wrapper">
+        <div id="products">
+          {products.map((product) => {
+            return (
+              <div
+                className="product"
+                key={product.id}
+              >
+                <img
+                  src={product.image}
+                  alt=""
+                  className="product-img"
+                />
+                <h3>{product.title}</h3>
+                <p className="price">${product.price}</p>
+              </div>
+            );
+          })}
+        </div>
+      </main>
     </div>
   );
 }
